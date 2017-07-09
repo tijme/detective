f665ae539193f6fcf67e768155ecfa60b886adce
Tijme Gommers <tijme@finnwea.com>
2017-07-09 12:52:02 +0200

    Initial commit.

---
+++ README.rst
@@ -0,0 +1,53 @@
+Detective
+---------
+
+
+.. image:: https://img.shields.io/pypi/pyversions/detective.svg
+   :target: https://www.python.org/
+   :alt: Python version
+
+.. image:: https://img.shields.io/pypi/v/detective.svg
+   :target: https://pypi.python.org/pypi/detective/
+   :alt: PyPi version
+
+.. image:: https://img.shields.io/pypi/l/detective.svg
+   :target: https://github.com/tijme/detective/blob/master/LICENSE.rst
+   :alt: License: MIT
+
+
+Detective helps you find information (at your favorite bug bounty program) that you are not supposed to see. It primarily focuses on information disclosure and sensitive data exposure vulnerabilities.
+
+1. Specify the program's scope.
+2. Run detective and wait.
+3. Profit!
+
+
+Table of contents
+-----------------
+
+-  `Installation <#installation>`__
+-  `Documentation <#documentation>`__
+-  `Issues <#issues>`__
+-  `License <#license>`__
+
+Installation
+------------
+
+First make sure you're on `Python 2.7/3.3 <https://www.python.org/>`__ or higher. Then run the command below to install Detective.
+
+``$ pip install --upgrade detective``
+
+Documentation
+-------------
+
+#ToDo
+
+Issues
+------
+
+Issues or new features can be reported via the GitHub issue tracker. Please make sure your issue or feature has not yet been reported by anyone else before submitting a new one.
+
+License
+-------
+
+Detective is open-sourced software licensed under the `MIT license <https://github.com/tijme/detective/blob/master/LICENSE.rst>`__.
---
+++ setup.py
@@ -0,0 +1,76 @@
+# -*- coding: utf-8 -*-
+
+# MIT License
+#
+# Copyright (c) 2017 Tijme Gommers
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+from setuptools import find_packages, setup
+from shutil import copyfile
+
+copyfile(".semver", "detective/.semver")
+
+with open("requirements.txt") as file:
+    requirements = file.read().splitlines()
+
+with open("README.rst") as file:
+    readme = file.read().replace(".svg?pypi=png.from.svg", ".png")
+
+with open(".semver") as file:
+    semver = file.read()
+
+setup(
+    version=semver,
+    name="detective",
+    description="A private detective that gathers information you're not supposed to know about",
+    long_description=readme,
+    keywords = ["vulnerability", "bug-bounty", "security", "information-disclosure", "sensitive-data-exposure", "scanner"],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Education",
+        "Intended Audience :: Information Technology",
+        "Intended Audience :: System Administrators",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Operating System :: MacOS",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 2.7",
+        "Topic :: Security"
+    ],
+    packages=find_packages(),
+    package_data={
+        "detective": [
+            ".semver"
+        ]
+    },
+    platforms=["any"],
+    author="Tijme Gommers",
+    author_email="tijme@finnwea.com",
+    license="MIT",
+    url="https://github.com/tijme/detective",
+    install_requires=requirements
+)
